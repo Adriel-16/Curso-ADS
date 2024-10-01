46a43141d375006a1e967637fb4f3c483367fb3e
Adriel-16 <adriel.docabelinho@gmail.com>
2024-10-01 17:18:55 -0300

    C

--- ADS-MODULO3/Nivelamento/nivelamento.C
+++ ADS-MODULO3/Nivelamento/nivelamento.C
@@ -1,9 +1,51 @@
 #include <stdio.h>
 
-int main(){
+double soma(double A, double B){
+    
+    return A + B;
+    
+}
 
-    int aoc = 1;
+double subtracao(double A, double B){
+    
+    return A - B;
+    
+}
 
+double multiplicacao(double A, double B){
+    
+    return A * B;
+    
+}
 
+double divisao(double A, double B){
+    
+    return A / B;
+    
+}
 
+double potencia(float a, floatb){
+    
+    
+    
+}
+
+double fat(float A){
+    
+    if(A == 0){
+        
+        return 1;
+        
+    }else{
+        
+        return A * fat (A-1);
+        
+    }
+    
+}
+
+int main() {
+    int numero = 5;
+    printf("Fatorial de %d é %.6lf\n", numero, fat(numero));
+    return 0;
 }
