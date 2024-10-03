5b63410faa79060a6d7d1af9e4704ff14dd21831
Adriel-16 <adriel.docabelinho@gmail.com>
2024-10-03 17:17:52 -0300

    C

--- ADS-MODULO3/Nivelamento/nivelamento.C
+++ ADS-MODULO3/Nivelamento/nivelamento.C
@@ -1,51 +1,140 @@
 #include <stdio.h>
 
-double soma(double A, double B){
-    
-    return A + B;
-    
+double soma(){
+
+    float A;
+    float B;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &A);
+    printf("\nDigite o segundo numero:\n");
+    scanf("%f", &B);
+
+    float resultado = A + B;
+
+    printf("O resultado da soma e: %.5f\n", resultado);
+    return resultado;
 }
 
-double subtracao(double A, double B){
-    
-    return A - B;
-    
+double subtracao(){
+
+    float A;
+    float B;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &A);
+    printf("\nDigite o segundo numero:\n");
+    scanf("%f", &B);
+
+    float resultado = A - B;
+
+    printf("O resultado da subtracao e: %.5f\n", resultado);
+    return resultado;
+
 }
 
-double multiplicacao(double A, double B){
-    
-    return A * B;
-    
+double multiplicacao(){
+
+    float A;
+    float B;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &A);
+    printf("\nDigite o segundo numero:\n");
+    scanf("%f", &B);
+
+    float resultado = A * B;
+
+    printf("O resultado da multiplicacao e: %.5f\n", resultado);
+    return resultado;
+
+
 }
 
-double divisao(double A, double B){
-    
-    return A / B;
-    
+double divisao(){
+
+    float A;
+    float B;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &A);
+    printf("\nDigite o segundo numero:\n");
+    scanf("%f", &B);
+
+    float resultado = A / B;
+
+    printf("O resultado da divisao e: %.5f\n", resultado);
+    return resultado;
+
+
 }
 
-double potencia(float a, floatb){
-    
-    
-    
-}
+double fat(){
 
-double fat(float A){
-    
-    if(A == 0){
-        
-        return 1;
-        
+    float A;
+    double result = 1.0;
+
+    printf("\ndigite o numero que voce queira saber o fatorial:(nao pode ser negativo)\n");
+    scanf("%f", &A);
+
+    if(A < 1){
+
+        printf("Fatorial nao funciona para numeros negativos");
+        return -1;
     }else{
-        
-        return A * fat (A-1);
-        
+
+        for(int i = 1; i <=A; i++){
+
+            result *= i;
+
+        }
+
     }
-    
+
+    printf("o fatorial de %f é: %f", A, reuslt);
+    return result;
+
 }
+
 
 int main() {
-    int numero = 5;
-    printf("Fatorial de %d é %.6lf\n", numero, fat(numero));
-    return 0;
+
+    char sair;
+    int escolha;
+
+    do{
+
+        printf("Faca a sua escolha: (Digite um numero para escolher)\n\n");
+        printf("1. soma\n");
+        printf("2. subtracao\n");
+        printf("3. multiplicacao\n");
+        printf("4. divisao\n");
+        printf("7. fatorial\n");
+        scanf("%d", &escolha);
+
+        if(escolha == 1){
+
+            soma();
+
+        }else if(escolha == 2){
+
+            subtracao();
+
+        }else if( escolha == 3){
+
+            multiplicacao();
+
+        }else if(escolha == 4){
+
+            divisao();
+
+        }else if(escolha == 7){
+
+            fat();
+
+        }
+
+    printf("\nvoce deseja sair?(digite N ou n para continuar)\n");
+    scanf(" %c", &sair);
+    }while(sair == 'N' || sair == 'n');
 }
